a4dcb8eaf89037cadcdf7ceee91ba9f9c0be0f28
Ghennadi Berezovschi <ghennadiberezovschi@gmail.com>
2024-04-10 17:39:20 +0100

    Task4 completed

---
+++ src/main/java/org/example/Task4.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Task4 {
+    public static void main(String[] args) {
+        compareNumbers();
+    }
+
+    public static void compareNumbers() {
+        int a = 8;
+        int b = 35;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
