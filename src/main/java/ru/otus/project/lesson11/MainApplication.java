04b3e21e8e9a7701b0da52a44e956fc31745ed3e
Ghennadi Berezovschi <ghennadiberezovschi@gmail.com>
2024-05-15 17:40:50 +0100

    Lesson-12 changed

--- src/main/java/ru/otus/project/lesson11/MainApplication.java
+++ src/main/java/ru/otus/project/lesson11/MainApplication.java
@@ -3,19 +3,19 @@ package ru.otus.project.lesson11;
 public class MainApplication {
     public static void main(String[] args) {
         Cat cat = new Cat("Barsik",10,6);
-        Horse horse = new Horse("Grom",20,300,4);
-        Dog dog = new Dog("Bobik",20,250,4);
+        Horse horse = new Horse("Grom",20,400,5);
+        Dog dog = new Dog("Bobik",20,250,5);
 
-        horse.run(200);
-        horse.swim(10);
-        horse.info();
+        horse.run(100);
+        horse.swim(20);
+
         System.out.println();
         dog.run(200);
         dog.swim(10);
-        dog.info();
+
         System.out.println();
         cat.run(200);
-        cat.info();
+        cat.swim();
 
     }
 }
