d4c740481f9d3e77f180c53d06b06d2b6666dd64
Ghennadi Berezovschi <ghennadiberezovschi@gmail.com>
2024-05-15 11:28:30 +0100

    Lesson-12 committed

---
+++ src/main/java/ru/otus/project/lesson12/Plate.java
@@ -0,0 +1,31 @@
+package ru.otus.project.lesson12;
+
+public class Plate {
+    private int maxFood;
+    private int currentFood;
+
+    public Plate(int maxFood) {
+        this.maxFood = maxFood;
+        this.currentFood = 0;
+
+    }public boolean spendFood(int foodAmount){
+        if(currentFood >= foodAmount){
+            currentFood -= foodAmount;
+
+            return true;
+        }
+        else return false;
+    }
+    public void addFood(int foodAmount) {
+        if (currentFood + foodAmount <= maxFood) {
+            currentFood += foodAmount;
+            System.out.println(foodAmount + " added to the plate");
+        } else {
+            System.out.println("Could not add the food. Plate is too small");
+        }
+    }
+
+}
+
+
+
